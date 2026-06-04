784fe5ff34e8a5a7bea6d1d247515615d97f8b27
cdwenz <cdwenz@gmail.com>
2026-06-04 13:47:41 -0300

    returnMeta true force

--- src/time-tracker/time-tracker.controller.ts
+++ src/time-tracker/time-tracker.controller.ts
@@ -56,11 +56,10 @@ export class TimeTrackerController {
     console.log("GET TIME: ", user)
     console.log("Query: ", query)
     
-    if (query.returnMeta === false) {
-      const response = await this.service.listFlat(user, query);
-      // console.log("Res-Flat: ", response);
-      return response;
-    }
+    // if (query.returnMeta === false) {
+    //   const response = await this.service.listFlat(user, query);
+    //   return response;
+    // }
     const response = await this.service.listWithMeta(user, query);
     // console.log("Res-Meta: ", response);
     return response;
